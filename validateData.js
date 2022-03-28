87c3afd94a860cde020cc9f27a379fdf5a47b911
Mariana Rocha Silva <marianarocha308@gmail.com>
2022-03-28 20:17:13 -0300

    req 5 ok

--- validateData.js
+++ validateData.js
@@ -37,7 +37,7 @@ const validateAge = (req, res, next) => {
 };
 
 const DateFormat = (date) => {
-  const regexValidation = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i;
+  const regexValidation = /^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$/;
   return regexValidation.test(date);
 };
 
@@ -56,12 +56,13 @@ const validateRate = (req, res, next) => {
   if (talk.rate < 1 || talk.rate > 5) {
     return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
   }
+  
   next(); 
 };
 
 const validateTalk = (req, res, next) => {
   const { talk } = req.body;
-  if (!talk || !talk.watchedAt || !talk.rate) {
+  if (!talk || !talk.watchedAt || talk.rate === undefined) {
     return res.status(400)
     .json({ message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' });
   }
